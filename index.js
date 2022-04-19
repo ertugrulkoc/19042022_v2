af85f60d8b03c219394b78708f582227b5bfee56
ertugrulkoc <ert0x@yandex.com>
2022-04-19 13:28:34 +0300

    a

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/env/somekeyerto', async (req, res) => {
+    const text= JSON.stringify(process.env.somekeyerto)
+    res.send(text);
+})
+
+app.get('/api/env/allkeys', async (req, res) => {
+    const text = JSON.stringify(process.env)
+    res.send(text)
+})
+
+app.get('/api/hello', async (req, res) => {
+
+    res.send('Hello World Test Erto v1')
+})
+
+app.get('/api/merhaba', (req, res) => {
+
+    res.send('Merhaba Dünya v1')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+    console.log(body)
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running v1')
+})
